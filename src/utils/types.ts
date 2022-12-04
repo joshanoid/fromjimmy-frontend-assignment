43090d5824f282e5a2274774cc30b585cb0f7697
Jozsef Varga <joshanoid@gmail.com>
2022-12-04 16:11:37 +0100

    added types

---
+++ src/utils/types.ts
@@ -0,0 +1,20 @@
+type VehicleType = {
+    IsPrimary: boolean
+    Name: string
+}
+
+type Manufacturer = {
+    Country: string
+    Mfr_CommonName: string
+    Mfr_ID: number
+    Mfr_Name: string
+    VehicleTypes: ReadonlyArray<VehicleType>
+}
+
+export type ManufacturersResponse = {
+    Count: number
+    Message: string
+    SearchCriteria: null
+    Results: ReadonlyArray<Manufacturer>
+    nextPage: number
+}
